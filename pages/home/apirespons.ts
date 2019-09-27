23d0d5f543fd680023f6162b9aceae46078f19f0
briny-hitesh <hitesh@brinyinfoway.com>
2019-09-27 06:40:29 +0530

    Make circle dynamic

---
+++ pages/home/apirespons.ts
@@ -0,0 +1,33 @@
+export const data = {
+  "current": {
+    "Group": 5, 
+    "Threshold": 20, 
+    "TotalNoOfTaskCompleted": 18
+  }, 
+  "previous": {
+    "1": {
+      "Group": 1, 
+      "Threshold": 20, 
+      "TotalNoOfTaskCompleted": 20
+    }, 
+    "2": {
+      "Group": 2, 
+      "Threshold": 20, 
+      "TotalNoOfTaskCompleted": 40
+    }, 
+    "3": {
+      "Group": 3, 
+      "Threshold": 20, 
+      "TotalNoOfTaskCompleted": 60
+    }, 
+    "4": {
+      "Group": 4, 
+      "Threshold": 20, 
+      "TotalNoOfTaskCompleted": 80
+    }
+  }, 
+  "upcoming": {
+    "Group": 6, 
+    "Threshold": 20
+  }
+}
--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,15 +1,52 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { data } from './apirespons';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
   styleUrls: ['home.scss']
 })
 export class HomePage {
-
+  circleCodes: {
+    left: number;
+    right: number;
+    center: number;
+    start: number;
+  } = {
+    left: 190,
+    right: 400,
+    center: 300,
+    start: 50
+  }
+  size = {
+    sm: 45,
+    lg: 60,
+    md: 50
+  }
+  data = data;
+  svgJson: Array<{size: number, color: string, enableClick: boolean, text: number, x: number, y: number}> = [];
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    console.log(data);
+    this.svgJson.push({
+      size: this.size.md,
+      color: '',
+      enableClick: false,
+      text: this.data['upcoming'].Group,
+      x: this.circleCodes.center,
+      y: this.circleCodes.start,
+    });
+    this.svgJson.push({
+      size: this.size.lg,
+      color: 'green',
+      enableClick: false,
+      text: this.data['current'].Group,
+      x: this.circleCodes.right,
+      y: this.circleCodes.start,
+    });
   }
 
 }
